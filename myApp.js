ecd0f0789f489e01f048d75dcdff0617836326c7
Marc <marctotaro94@gmail.com>
2024-11-12 12:47:20 -0600

    feat: Sendig view/index.html

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
-
+console.log("Hello world");
+app.get("/", function(req, res) {
+ // res.send("Hello Express");
+ res.sendFile(__dirname + '/views/index.html')
+});
 
 
 
